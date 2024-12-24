6ee8b7952c13dd8ab213d47f3b9029dcedb94d1e
Mayur79 <mayurbhoyar4455@gmail.com>
2024-12-24 19:08:16 +0530

    taxsavingupdated

---
+++ frontend/src/api/professionalIncome.ts
@@ -0,0 +1,47 @@
+import axios from "axios"
+
+export const fetchProfessionalData = async (token: string) => {
+    try {
+     
+      const response = await axios.get(
+        `${import.meta.env.VITE_BACKEND_URL}/api/v1/fillDetail/getProfesionalIncomeData`,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      )
+  
+      if (response.data) {
+        return response.data
+      }
+      return []
+    } catch (error) {
+      console.error(`Error fetching data `, error)
+      console.log("eror",error);
+   
+    }
+  }
+
+export const fetchBussinessData = async (token: string) => {
+    try {
+     
+      const response = await axios.get(
+        `${import.meta.env.VITE_BACKEND_URL}/api/v1/fillDetail/getBussinessIncomeData`,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      )
+  
+      if (response.data) {
+        return response.data
+      }
+      return []
+    } catch (error) {
+      console.error(`Error fetching data `, error)
+      console.log("eror",error);
+   
+    }
+  }
